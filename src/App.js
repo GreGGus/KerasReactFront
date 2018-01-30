95715b95711ce8bb94b17ef1ff42e3dfd35d4c41
GreGGus <gportier@ece.fr>
2018-01-30 16:51:20 +0100

    -front ok. -modal to fix

--- src/App.js
+++ src/App.js
@@ -3,49 +3,87 @@ import logo from './logo.svg';
 import './App.css';
 import ImageUploader from 'react-images-upload';
 import axios from 'axios';
-
+import Loadable from 'react-loading-overlay'
 
 class App extends Component {
 
   constructor(props){
   		super(props);
-  		 this.state = { pictures: [] };
+  		 this.state = { firstTry:true, pictures: [] ,results:null,response:null,isActive:false};
   		 this.onDrop = this.onDrop.bind(this);
+  		 this.changeModal = this.changeModal.bind(this);
+         }
 
-  }
   onDrop(picture) {
-  console.log("POST PICTURE")
-  const url = "http://176.31.251.185:5000"
-              axios.post(url, picture, {
-                headers: {
-                  'Content-Type': picture.type
-                }
-              });
-  		this.setState({
-              pictures: this.state.pictures.concat(picture),
-          });
+      const url = "http://176.31.251.185:5000/predict"
+        let data = new FormData();
+        var self = this;
+        for (var i = 0; i < picture.length; i++) {
+                let file = picture.item(i);
+                data.append('image', file, file.name);
+            }
+         axios.post(url, data, {headers: {'Content-Type': 'multipart/form-data'}
+                  }).then(response => {
+                      console.log('Success', response);
+                      self.changeModal(response)
+                    }).catch(error => {
+                      console.log('Error', error);
+                      self.setState({isActive:false})
+
+                    });
+            let newPictures=[]
+            newPictures.concat(picture)
+            self.changePicture(newPictures)
   	}
 
+   changeModal(response){
+                this.setState({
+                          isActive:false,
+                          results:response.data,
+                          firstTry:false
+           })
+   }
+
+  changePicture(newPictures){
+  	this.setState({
+                pictures: newPictures,
+                isActive:true
+            });
+  }
+
   render() {
+
+  const {firstTry,isActive,results} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Keras DeepLearing Picture</h1>
-        </header>
-          <ImageUploader
-                        	withIcon={true}
-                        	buttonText='Choose images'
-                        	onChange={this.onDrop}
-                        	imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                        	maxFileSize={5242880}
-                    />
-        <p className="App-intro">
-          Upload picture to try
-        </p>
-      </div>
+              <Loadable
+
+                    active={isActive}
+                    spinner
+                    animate
+                    zIndex={1000}
+                    text='Processing image'
+                    >
+                  <div className="App">
+                                <header className="App-header">
+                                  <img src={logo} className="App-logo" alt="logo" />
+                                  <h1 className="App-title">Keras DeepLearing Image</h1>
+                                </header>
+                                <ImageUploader
+                                                buttonText='Choose images'
+                                                onChange={this.onDrop}
+                                                imgExtension={['.jpg','.png']}
+                                                maxFileSize={52428800}
+                                        />
+                                 { firstTry ? <p className="App-intro"> Upload picture to try</p> : ""}
+                                 { !firstTry ? (results.predictions.map(result => <ResultItem result={result} />)) : ""}
+
+                  </div>
+              </Loadable>
     );
   }
 }
+
+const ResultItem = (result) => <p>  <div> {result.result.label} :   {Math.round(result.result.probability * 100 )} % </div> </p>
+
 
 export default App;
